6ce2a3852c78183c2989a22f45a61a40e4263771
pafonator <pafonator@gmail.com>
2026-02-14 16:54:53 +0100

    latest working versions for dependencies

--- src/main.rs
+++ src/main.rs
@@ -32,41 +32,6 @@ fn run(state: &UtttState, timeout: std::time::Duration) -> Option<(GridSlot, Gri
     return best_move;
 }
 
-// fn main() {
-//     // Initialize the logger with a default level
-//     env_logger::Builder::new()
-//         .filter_level(log::LevelFilter::Debug)
-//         .target(env_logger::Target::Stdout) 
-//         .init();
-    
-//     // Get the command-line arguments
-//     let args: Vec<String> = env::args().collect();
-
-//     let state: UtttState;
-//     let duration :u64;
-//     if args.len() == 1 {
-//         // Look for optional --state argument
-//         duration = 10;
-//         state = UtttState::default();
-//     } else if args.len() != 3 {
-//         eprintln!("Usage: {} <Duration> <UtttState_JSON>", args[0]);
-//         process::exit(1);
-//     } else {
-//         duration = args[1].parse::<u64>().expect("Failed to parse duration");
-//         let input_json = &args[2]; // The second argument is the JSON input
-
-//         debug!("Getting root Node from input JSON");
-//         // Deserialize the input JSON into UtttState
-//         state = serde_json::from_str(&input_json).expect("Failed to deserialize UtttState");
-//     }
-    
-//     let action = run(&state, Duration::new(duration,0));
-
-//     let json = serde_json::to_string(&action).unwrap();
-//     info!("Serialized best move:\n");
-//     println!("[RESULT] {}", json);
-// }
-
 fn main() {
     console_error_panic_hook::set_once();
 
